a54992ec8e59c04d11781d3e239e4ac6e8cc18a9
JohnPiatras <john.piatras@gmail.com>
2019-11-01 16:58:15 +0000

    Added a background circle to clock, added start and stop buttons

--- pomodoro.js
+++ pomodoro.js
@@ -10,11 +10,11 @@ function Pomodoro() {
     this.currentTime = this.workDuration;
     this.activity = "work";   //can be 'work' or 'break'
     this.intervalID = null;
-    this.tickInterval = 10; //1 sec
+    this.tickInterval = 1000; //1 sec
 
     this.reset = function(){
         this.pause();
-        this.currentTime = this.sessionDuration;
+        this.currentTime = this.workDuration;
         this.activity = "work";
     };
     this.pause = function(){
@@ -30,6 +30,7 @@ function Pomodoro() {
     };
     this.tick = function(){
         if(this.currentTime == 0){
+          
             switch(this.activity){
                 case "work":
                     this.activity = "break";
@@ -39,11 +40,13 @@ function Pomodoro() {
                     this.activity = "work";
                     this.currentTime = this.workDuration;
                     break;
-            }
+            }               
+        }else{
+            this.currentTime -= 1;   
+            
         }
-            this.currentTime -= 1;
+        if(this.updateCallback)this.updateCallback(this);                             
         
-        if(this.updateCallback)this.updateCallback(this);
     };
     this.setUpdateCallback = function(callback){
         this.updateCallback = callback;
@@ -79,60 +82,72 @@ function Pomodoro() {
 
 };
 
-
-
 function PomodoroDrawer(canvas, textEl){
     this.canvas = canvas;
     this.textEl = textEl;
-    this.clear = true;
+    this.ctx = this.canvas.getContext('2d');
+   // this.clear = true;
+
+    this.clear = function(color){
+        let ctx = this.ctx;
+        let r = this.canvas.height / 2;
+        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height); 
+        ctx.globalAlpha = 0.2;
+        ctx.beginPath();
+        ctx.moveTo(r,r);
+        ctx.arc(r, r, r, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.globalAlpha = 1;              
+    };
 
     this.draw = function(pomo_obj){
         //check and fix canvas size if needed
         if(this.canvas.width !== this.canvas.clientWidth){
             this.canvas.width = this.canvas.clientWidth;
-            this.canvas.height = this.canvas.clientHeight;
+            this.canvas.height = this.canvas.clientHeight;            
         }
 
         let r = this.canvas.height / 2;
-        let ctx = this.canvas.getContext('2d');
+        let ctx = this.ctx;
         let t = 1 - pomo_obj.getTimeFraction();
         let t_sec = pomo_obj.getTime();
-        //console.log(t);
-        if(this.clear == true) {
-            ctx.clearRect(0,0, r * 2, r * 2);
-            this.clear = false;
-            ctx.beginPath();
-            ctx.arc(r, r, r, 0, 2 * Math.PI);
-            ctx.stroke();
-        }
-        ctx.beginPath();
         
         let start_angle = 1.5 * Math.PI;
         let end_angle = 0;
 
-        if(t == 1){
-            start_angle = 0;
-            end_angle = 2 * Math.PI;
-            this.clear = true;
+        if(t == 0){                         
+            if(pomo_obj.getActivity() == "work"){
+                this.clear("red");
+            }else{
+                this.clear("green");
+            }
+                          
         }else{
+            if(t == 1){
+                start_angle = 0;
+                end_angle = 2 * Math.PI;
+            }else{
+                end_angle = (t * 2 * Math.PI) - 0.5 * Math.PI;
+            }
+            ctx.beginPath();
             ctx.moveTo(r,r);
             ctx.lineTo(r , 0);
-            start_angle = 1.5 * Math.PI;
-            end_angle = (t * 2 * Math.PI) - 0.5 * Math.PI;
+
+            ctx.arc(r, r, r, start_angle, end_angle);
+            ctx.lineTo(r, r);
+            if(pomo_obj.getActivity() == "work"){
+                ctx.fillStyle = 'red';
+            }else{
+                ctx.fillStyle = 'green';
+            }
+            ctx.fill();
         }
-        ctx.arc(r, r, r, start_angle, end_angle);
-        ctx.lineTo(r, r);
-        if(pomo_obj.getActivity() == "work"){
-            ctx.fillStyle = 'red';
-        }else{
-            ctx.fillStyle = 'green';
-        }
-        ctx.fill();
 
         let t_min = Math.floor(t_sec / 60);
         let t_sec_remainder = t_sec - t_min * 60;
-        textEl.textContent = ((t_min < 10) ? "0":"") + t_min + ":" + ((t_sec_remainder < 10) ? "0":"") + t_sec_remainder;
-        //ctx.stroke();
+
+        textEl.textContent = ((t_min < 10) ? "0":"") + t_min + ":" + ((t_sec_remainder < 10) ? "0":"") + t_sec_remainder;       
     };
 };
 
@@ -147,4 +162,29 @@ let printUpdate = function(pomo_obj){
 
 
 pomo.setUpdateCallback(pomoDrawer.draw.bind(pomoDrawer));
-pomo.start();
+pomo.reset();
+pomoDrawer.draw(pomo);
+
+let bttn_start = document.getElementById("bttn-start");
+bttn_start.onclick = function(){
+    if(bttn_start.innerHTML == "<span>Start</span>"){
+        //pomoDrawer.clear = true;
+        pomo.start();
+        bttn_start.innerHTML = "<span>Stop</span>";
+    }else{
+        pomo.pause();
+        bttn_start.innerHTML = "<span>Start</span>";
+    }
+}
+
+let bttn_reset = document.getElementById("bttn-reset");
+bttn_reset.onclick = function(){    
+    pomo.reset();
+    pomoDrawer.draw(pomo);    
+    bttn_start.innerHTML = "<span>Start</span>";
+}
+//pomo.start();
+
+
+
+
